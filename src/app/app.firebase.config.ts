835267a924cc748765dc00f1ff2a03adba172d47
Shubham Kondekar <kondekarshubham123@gmail.com>
2025-12-30 11:11:50 +0530

    initial code for expense tracker has been added

---
+++ src/app/app.firebase.config.ts
@@ -0,0 +1,10 @@
+export const firebaseConfig = {
+    apiKey: "AIzaSyDMJncEUVvGi74YJIaz1iPOgkl9xYI2Tsc",
+    authDomain: "coinly-ai.firebaseapp.com",
+    projectId: "coinly-ai",
+    databaseURL: 'https://coinly-ai-default-rtdb.firebaseio.com/',
+    storageBucket: "coinly-ai.firebasestorage.app",
+    messagingSenderId: "761790592642",
+    appId: "1:761790592642:web:6c8437a7252733751f269b",
+    measurementId: "G-H1HEFMNY3D"
+  }
